cb5af11df324a612f623c3537255339c8c9e761c
Akram Kassay <samuraijourney@hotmail.com>
2015-03-08 23:56:01 -0400

    Got head tracking of character to match oculus head angle

---
+++ Assets/Scripts/Character/HeadTracker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadTracker : MonoBehaviour {
+
+	private GameObject _ovrController;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_ovrController = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.rotation = _ovrController.transform.rotation;
+		transform.Rotate (new Vector3 (0, -90, -90));
+	}
+}
