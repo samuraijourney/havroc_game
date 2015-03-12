0abffb6d18a13bff937d38ba9569eabf0b13cdfc
Akram Kassay <samuraijourney@hotmail.com>
2015-03-11 23:49:34 -0400

    Updated head tracker

--- Assets/Scripts/Character/HeadTracker.cs
+++ Assets/Scripts/Character/HeadTracker.cs
@@ -3,20 +3,20 @@ using System.Collections;
 
 public class HeadTracker : MonoBehaviour {
 
-	private GameObject m_ovrController;
+	private GameObject m_ovrCentreAnchor;
 	private GameObject m_havrocPlayer;
 
 	// Use this for initialization
 	void Start () 
 	{
-		m_ovrController = GameObject.FindGameObjectWithTag ("MainCamera");
+		m_ovrCentreAnchor = GameObject.Find ("CenterEyeAnchor");
 		m_havrocPlayer = GameObject.Find ("Havroc Player");
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.rotation = m_ovrController.transform.rotation;
+		transform.rotation = m_ovrCentreAnchor.transform.rotation;
 		transform.Rotate (new Vector3 (0, 90, -90));
 
 		if(Mathf.Abs(transform.localEulerAngles.x) > 90.0f)
