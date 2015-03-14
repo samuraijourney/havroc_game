d3632b4be9e2103deb899cf5519ab6b3a5f4090b
Akram Kassay <samuraijourney@hotmail.com>
2015-03-14 03:16:04 -0400

    Removing unecessary code from initializer script

--- Assets/Scripts/Common/Initializer.cs
+++ Assets/Scripts/Common/Initializer.cs
@@ -17,7 +17,6 @@ public class Initializer : MonoBehaviour
 			if(startConnection)
 			{
 				HVR_Network.RegisterConnectCallback(this.OnConnect);
-				//HVR_Network.RegisterDisconnectCallback(this.OnDisconnect);
 				
 				// Initialize network connection
 				if (!HVR_Network.IsActive()) 
@@ -46,20 +45,14 @@ public class Initializer : MonoBehaviour
 					}
 				}
 			}
-			else
-			{
-				Cleanup();
-			}
 		}
 	}
 
-	// Use this for initialization
 	public void Start () 
 	{
 
 	}
 		
-	// Update is called once per frame
 	public void Update () 
     {
 		if(startTracking && HVR_Network.IsActive() && !HVR_Tracking.IsActive())
@@ -67,31 +60,26 @@ public class Initializer : MonoBehaviour
 			HVR_Tracking.StartTrackingService();
 		}
 	}
-	
+
+	public void OnConnect()
+	{
+		Debug.Log("Connect successful");
+	}
+
+	public void OnDestroy()
+	{
+		Cleanup();
+	}
+
 	private void Cleanup()
 	{
 		if(useDLL)
 		{
 			Debug.Log("Ending tracking");
 			HVR_Tracking.EndTrackingService();
-
+			
 			Debug.Log("Ending connection");
 			HVR_Network.EndConnection();
 		}
-	}
-
-	public void OnConnect()
-	{
-		Debug.Log("Connect successful");
-	}
-	
-	public void OnDisconnect()
-	{
-		Debug.Log("Disconnect successful");
-	}
-
-	public void OnDestroy()
-	{
-		Cleanup();
 	}
 }
